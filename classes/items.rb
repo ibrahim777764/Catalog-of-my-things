726cae747904d538c364acc81e2c0ec2f2dec154
lomantech <87063237+lomantech@users.noreply.github.com>
2022-09-13 07:52:16 +0300

    Add linters error config file

--- classes/items.rb
+++ classes/items.rb
@@ -1,31 +1,29 @@
 require 'date'
 class Items
-    attr_accessor :author, :label, :genre, :source
-    attr_reader :id
+  attr_accessor :author, :label, :genre, :source
+  attr_reader :id
+
   def initialize(publish_date)
-    @id = rand(1..10000)
+    @id = rand(1..10_000)
     @publish_date = publish_date
     @archived = false
-    @author =author
+    @author = author
     @label = label
     @genre = genre
     @source = source
   end
 
   def move_to_archive()
-    #@archived = true if can_be_archived? true
-   if can_be_archived? == true
-      @archived = true
-   end
+    @archived = true if can_be_archived? == true
   end
 
   private
+
   def can_be_archived?()
     current_date = Date.today.year
-    finalDate = current_date - @publish_date
-    finalDate > 10? true : false
+    final_date = current_date - @publish_date
+    final_date > 10
   end
-
 end
 
 b = Items.new(2020)
