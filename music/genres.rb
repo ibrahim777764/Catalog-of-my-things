ab939447709e967dac76f542c958f57e78ebe305
lomantech <87063237+lomantech@users.noreply.github.com>
2022-09-14 18:15:41 +0300

    add genre file with class

---
+++ music/genres.rb
@@ -0,0 +1,13 @@
+require_relative '../classes/items'
+class Genres < Items
+  attr_reader :id, :items
+  def initialize(name)
+    @id = rand(1..1000)
+    @name = name
+    @items =[]
+  end
+  def add_item(item)
+    @items << item
+    item.add_genre(self)
+  end
+end
