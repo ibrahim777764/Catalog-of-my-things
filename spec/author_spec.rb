63cd67801c0121e1ed01dec1cb8d73e51b683b79
Shubh <shubh.scb@gmail.com>
2022-09-14 13:52:54 +0530

    GAME AND AUTHOR ALL TEST PASS

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,10 +1,33 @@
 require_relative '../classes/author'
+require_relative '../classes/items'
 
 describe Author do
   let(:author) { Author.new('Shubh', 'Dev') }
-  describe '#add_item' do
-    it 'returns true if item is added' do
-      expect(author.add_item('foo')).to be true
-    end
+  let(:item) { Items.new('game') }
+
+  it 'should have an id' do
+    expect(author.id).to be_an(Integer)
   end
+
+  it 'should have a first name' do
+    expect(author.first_name).to eq('Shubh')
+  end
+
+  it 'should have a last name' do
+    expect(author.last_name).to eq('Dev')
+  end
+
+  it 'should have items' do
+    expect(author.items).to be_an(Array)
+  end
+
+  it 'should add an item' do
+    author.add_item(item)
+    expect(author.items).to include(item)
+  end
+
+
 end
+
+
+
