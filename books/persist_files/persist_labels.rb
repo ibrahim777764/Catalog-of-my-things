4f4aa2817941e87c783dc957a998d4215ab90710
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-09-14 12:54:25 +0500

    Add choice for the user between existing lable or creating new one

--- books/persist_files/persist_labels.rb
+++ books/persist_files/persist_labels.rb
@@ -1,1 +1,24 @@
-[]
+require 'json'
+module LabelsPersistence
+  def store_labels(labels)
+    data = []
+    file = './JSON_files/labels.json'
+    return unless File.exist?(file)
+
+    labels.each do |label|
+      data << { title: label.title, color: label.color }
+    end
+    File.write(file, JSON.generate(data))
+  end
+
+  def load_labels
+    data = []
+    file = './JSON_files/labels.json'
+    return data unless File.exist?(file) && File.read(file) != ''
+
+    JSON.parse(File.read(file)).each do |label|
+      data << Label.new(label['title'], label['color'])
+    end
+    data
+  end
+end
