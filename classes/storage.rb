87e1ecd65861fe26f079e4aa23f405b3f67e8847
lomantech <87063237+lomantech@users.noreply.github.com>
2022-09-16 10:45:03 +0300

    update Json files

--- classes/storage.rb
+++ classes/storage.rb
@@ -15,20 +15,20 @@ module Storage
     @games = [] if @games.nil?
     games = @games.map { |game| [game.multiplayer, game.last_played_at, game.publish_date] }
     source = JSON.dump(games)
-    File.write('games.json', source)
+    File.write('./json_files/games.json', source)
   end
 
   def dump_authors
     @authors = [] if @authors.nil?
     authors = @authors.map { |author| [author.first_name, author.last_name] }
     source = JSON.dump(authors)
-    File.write('authors.json', source)
+    File.write('./json_files/authors.json', source)
   end
 
   def load_authors
-    return unless File.exist?('authors.json')
+    return unless File.exist?('./json_files/authors.json')
 
-    authors = JSON.parse(File.read('authors.json'))
+    authors = JSON.parse(File.read('./json_files/authors.json'))
     authors.each do |author|
       obj = Author.new(author[0], author[1])
       @authors << obj
@@ -36,12 +36,11 @@ module Storage
   end
 
   def load_game
-    return unless File.exist?('games.json')
+    return unless File.exist?('./json_files/games.json')
 
-    games = JSON.parse(File.read('games.json'))
+    games = JSON.parse(File.read('./json_files/games.json'))
     games.each do |game|
       obj = Game.new(game[0], game[1], game[2])
-      # obj.id = game.first
       @games << obj
     end
   end
